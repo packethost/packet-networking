ef5589a20f65f5fbdedc994135f1010bc070a603
Mike Mason <mason@packet.com>
2019-08-07 13:11:10 -0500

    add docstring to existing tests

--- packetnetworking/distros/redhat/test_bonded.py
+++ packetnetworking/distros/redhat/test_bonded.py
@@ -2,13 +2,14 @@ from ... import utils
 from textwrap import dedent
 
 
-def test_bonded_tasks(mockit, redhat_bonded_network):
+def test_bonded_tasks(redhat_bonded_network):
     """Checks the expected number of tasks are created"""
     builder = redhat_bonded_network()
     assert len(builder.tasks) == 14
 
 
-def test_public_bonded_task_etc_sysconfig_network(mockit, redhat_bonded_network):
+def test_public_bonded_task_etc_sysconfig_network(redhat_bonded_network):
+    """Validates /etc/sysconfig/network for a public bond"""
     builder = redhat_bonded_network(public=True)
     tasks = builder.render()
     result = dedent(
@@ -23,7 +24,8 @@ def test_public_bonded_task_etc_sysconfig_network(mockit, redhat_bonded_network)
     assert tasks["etc/sysconfig/network"] == result
 
 
-def test_private_bonded_task_etc_sysconfig_network(mockit, redhat_bonded_network):
+def test_private_bonded_task_etc_sysconfig_network(redhat_bonded_network):
+    """Validates /etc/sysconfig/network for a private only bond"""
     builder = redhat_bonded_network(public=False)
     tasks = builder.render()
     result = dedent(
