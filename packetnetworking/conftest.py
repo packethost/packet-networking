6cf266d904bd55c8265d76f66e13e4aa92f088eb
Mike Mason <mason@packet.com>
2019-08-07 10:38:08 -0500

    convert conftest functions to pytest fixtures

--- packetnetworking/conftest.py
+++ packetnetworking/conftest.py
@@ -6,88 +6,94 @@ import pytest
 import random
 from .metadata import Metadata
 
-fake = faker.Faker()
+@pytest.fixture
+def fake():
+    return faker.Faker()
+
+@pytest.fixture
+def patch_list(patch_dict):
+    def _patch_list(orig, updates):
+        if isinstance(updates, list):
+            updates = {i: updates[i] for i in range(len(updates))}
+        for k, v in updates.items():
+            delete = str(k).startswith("-")
+            replace = str(k).startswith("!")
+            if delete or replace:
+                k = int(k[1:])
+            if delete:
+                del orig[k]
+                continue
+            if k >= len(orig) or replace:
+                orig += [None] * (k - len(orig) + 1)
+                orig[k] = v
+            elif isinstance(orig[k], list) and (isinstance(v, list) or isinstance(v, dict)):
+                _patch_list(orig[k], v)
+            elif isinstance(orig[k], dict) and isinstance(v, dict):
+                patch_dict(orig[k], v)
+            else:
+                orig[k] = v
+        return orig
+    return _patch_list
+
+@pytest.fixture
+def patch_dict(patch_list):
+    def _patch_dict(orig, updates):
+        for k, v in updates.items():
+            delete = str(k).startswith("-")
+            replace = str(k).startswith("!")
+            if delete or replace:
+                k = k[1:]
+            if delete:
+                del orig[k]
+                continue
+            if k not in orig or replace:
+                orig[k] = v
+            elif isinstance(orig[k], dict) and isinstance(v, dict):
+                _patch_dict(orig[k], v)
+            elif isinstance(orig[k], list) and (isinstance(v, list) or isinstance(v, dict)):
+                patch_list(orig[k], v)
+            else:
+                orig[k] = v
+        return orig
+    return _patch_dict
 
 
-def patch_list(orig, updates):
-    if isinstance(updates, list):
-        updates = {i: updates[i] for i in range(len(updates))}
-    for k, v in updates.items():
-        delete = str(k).startswith("-")
-        replace = str(k).startswith("!")
-        if delete or replace:
-            k = int(k[1:])
-        if delete:
-            del orig[k]
-            continue
-        if k >= len(orig) or replace:
-            orig += [None] * (k - len(orig) + 1)
-            orig[k] = v
-        elif isinstance(orig[k], list) and (isinstance(v, list) or isinstance(v, dict)):
-            patch_list(orig[k], v)
-        elif isinstance(orig[k], dict) and isinstance(v, dict):
-            patch_dict(orig[k], v)
-        else:
-            orig[k] = v
-    return orig
-
-
-def patch_dict(orig, updates):
-    for k, v in updates.items():
-        delete = str(k).startswith("-")
-        replace = str(k).startswith("!")
-        if delete or replace:
-            k = k[1:]
-        if delete:
-            del orig[k]
-            continue
-        if k not in orig or replace:
-            orig[k] = v
-        elif isinstance(orig[k], dict) and isinstance(v, dict):
-            patch_dict(orig[k], v)
-        elif isinstance(orig[k], list) and (isinstance(v, list) or isinstance(v, dict)):
-            patch_list(orig[k], v)
-        else:
-            orig[k] = v
-    return orig
-
-
-def _fake_address(**opts):
-    address = patch_dict(
-        {
-            "address": None,
-            "address_family": 4,
-            "cidr": None,
-            "gateway": None,
-            "management": True,
-            "netmask": None,
-            "network": None,
-            "public": True,
-        },
-        opts,
-    )
-    if address["address"]:
-        ip = address["address"]
-    elif address["address_family"] == 4:
-        ip = fake.ipv4(private=not address["public"])
-        if not address["cidr"]:
-            address["cidr"] = 31
-    else:
-        ip = fake.ipv6()
-        if not address["cidr"]:
-            address["cidr"] = 127
-    network = IPNetwork("{}/{}".format(ip, address["cidr"]))
-    gw_index = 0 if address["cidr"] in [31, 127] else 1
-    address["netmask"] = str(network.netmask)
-    address["network"] = str(network[0])
-    address["gateway"] = str(network[gw_index])
-    if not address["address"]:
-        address["address"] = str(network[gw_index + 1])
-    return address
 
 
 @pytest.fixture
-def fake_address():
+def fake_address(fake, patch_dict):
+    def _fake_address(**opts):
+        address = patch_dict(
+            {
+                "address": None,
+                "address_family": 4,
+                "cidr": None,
+                "gateway": None,
+                "management": True,
+                "netmask": None,
+                "network": None,
+                "public": True,
+            },
+            opts,
+        )
+        if address["address"]:
+            ip = address["address"]
+        elif address["address_family"] == 4:
+            ip = fake.ipv4(private=not address["public"])
+            if not address["cidr"]:
+                address["cidr"] = 31
+        else:
+            ip = fake.ipv6()
+            if not address["cidr"]:
+                address["cidr"] = 127
+        network = IPNetwork("{}/{}".format(ip, address["cidr"]))
+        gw_index = 0 if address["cidr"] in [31, 127] else 1
+        address["netmask"] = str(network.netmask)
+        address["network"] = str(network[0])
+        address["gateway"] = str(network[gw_index])
+        if not address["address"]:
+            address["address"] = str(network[gw_index + 1])
+        return address
     return _fake_address
 
 plan_slugs = (
@@ -100,42 +106,36 @@ plan_slugs = (
     "x2.xlarge.x86",
 )
 
-def _metadata(options, public=True):
-    addresses = []
-    if public:
-        addresses.append(_fake_address())
-        addresses.append(_fake_address(management=False))
-        addresses.append(_fake_address(
-            management=False,
-            address_family=6,
-            netmask="ffff:ffff:ffff:ffff:ffff:ffff:ffff:fffe",
-        ))
-    addresses.append(_fake_address(public=False))
 
-    return Metadata(
-        patch_dict(
-            {
-                "hostname": fake.hostname(),
-                "id": fake.uuid4(),
-                "plan": random.choice(plan_slugs),
-                "network": {
-                    "bonding": {"mode": 4},
-                    "interfaces": None,
-                    "addresses": addresses,
+@pytest.fixture
+def metadata(fake, fake_address, patch_dict):
+    def _metadata(options, public=True):
+        addresses = []
+        if public:
+            addresses.append(fake_address())
+            addresses.append(fake_address(management=False))
+            addresses.append(fake_address(
+                management=False,
+                address_family=6,
+                netmask="ffff:ffff:ffff:ffff:ffff:ffff:ffff:fffe",
+            ))
+        addresses.append(fake_address(public=False))
+
+        return Metadata(
+            patch_dict(
+                {
+                    "hostname": fake.hostname(),
+                    "id": fake.uuid4(),
+                    "plan": random.choice(plan_slugs),
+                    "network": {
+                        "bonding": {"mode": 4},
+                        "interfaces": None,
+                        "addresses": addresses,
+                    },
                 },
-            },
-            options,
+                options,
+            )
         )
-    )
-
-
-@pytest.fixture
-def metadata():
-    return _metadata
-
-@pytest.fixture
-def faker():
-    return fake
 
 @pytest.fixture
 def mockit():
