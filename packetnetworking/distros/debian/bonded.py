23f2a54f445f60e6d88eef3413894fbc74e45c72
Dustin Miller <dustin@packet.com>
2022-01-31 12:03:27 -0700

    fix: lets use udev rules for Debian 9 and beyond
    packet_lsb_release was returning unexpected values for Bullseye, lets 
    just expand this to all the non-EOL Debians and test accordingly

--- packetnetworking/distros/debian/bonded.py
+++ packetnetworking/distros/debian/bonded.py
@@ -16,7 +16,7 @@ class DebianBondedNetwork(NetworkBuilder):
 
         os = self.metadata.operating_system
 
-        if os.distro == "debian" and os.version in ["10", "11"]:
+        if os.distro == "debian" and os.version not in ["7", "8"]:
             self.tasks.update(generate_persistent_names())
         elif os.distro == "ubuntu" and os.version in [
             "14.04",
