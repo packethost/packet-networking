e905af59ba38f37b723dffe7b74e0c9fdabb2adb
Manuel Mendez <github@i.m.mmlb.dev>
2022-12-16 16:03:44 -0500

    Use 22.04 for test_ubuntu_2204_individual.py
    Probably a copy/paste that was not updated.

--- packetnetworking/distros/debian/test_ubuntu_2204_individual.py
+++ packetnetworking/distros/debian/test_ubuntu_2204_individual.py
@@ -4,19 +4,19 @@ import pytest
 
 
 @pytest.fixture
-def ubuntu_2104_individual_network(generic_debian_individual_network):
+def ubuntu_2204_individual_network(generic_debian_individual_network):
     def _builder(**kwargs):
-        return generic_debian_individual_network("ubuntu", "21.04", **kwargs)
+        return generic_debian_individual_network("ubuntu", "22.04", **kwargs)
 
     return _builder
 
 
-def test_ubuntu_2104_public_individual_task_etc_network_interfaces(
-    ubuntu_2104_individual_network,
+def test_ubuntu_2204_public_individual_task_etc_network_interfaces(
+    ubuntu_2204_individual_network,
 ):
     """Validates /etc/network/interfaces for a public bond"""
 
-    builder = ubuntu_2104_individual_network(public=True)
+    builder = ubuntu_2204_individual_network(public=True)
     tasks = builder.render()
     result = dedent(
         """\
@@ -52,14 +52,14 @@ def test_ubuntu_2104_public_individual_task_etc_network_interfaces(
     assert tasks["etc/network/interfaces"] == result
 
 
-def test_ubuntu_2104_private_individual_task_etc_network_interfaces(
-    ubuntu_2104_individual_network,
+def test_ubuntu_2204_private_individual_task_etc_network_interfaces(
+    ubuntu_2204_individual_network,
 ):
     """
     When no public ip is assigned, we should see the private ip details in the
     /etc/network/interfaces file.
     """
-    builder = ubuntu_2104_individual_network(public=False)
+    builder = ubuntu_2204_individual_network(public=False)
     tasks = builder.render()
     result = dedent(
         """\
@@ -83,12 +83,12 @@ def test_ubuntu_2104_private_individual_task_etc_network_interfaces(
     assert tasks["etc/network/interfaces"] == result
 
 
-def test_ubuntu_2104_public_individual_task_etc_network_interfaces_with_custom_private_ip_space(
-    ubuntu_2104_individual_network,
+def test_ubuntu_2204_public_individual_task_etc_network_interfaces_with_custom_private_ip_space(
+    ubuntu_2204_individual_network,
 ):
     """Validates /etc/network/interfaces for a public bond"""
     subnets = {"private_subnets": ["192.168.5.0/24", "172.16.0.0/12"]}
-    builder = ubuntu_2104_individual_network(public=True, metadata=subnets)
+    builder = ubuntu_2204_individual_network(public=True, metadata=subnets)
     tasks = builder.render()
     result = dedent(
         """\
@@ -126,15 +126,15 @@ def test_ubuntu_2104_public_individual_task_etc_network_interfaces_with_custom_p
     assert tasks["etc/network/interfaces"] == result
 
 
-def test_ubuntu_2104_private_individual_task_etc_network_interfaces_with_custom_private_ip_space(
-    ubuntu_2104_individual_network,
+def test_ubuntu_2204_private_individual_task_etc_network_interfaces_with_custom_private_ip_space(
+    ubuntu_2204_individual_network,
 ):
     """
     When no public ip is assigned, we should see the private ip details in the
     /etc/network/interfaces file.
     """
     subnets = {"private_subnets": ["192.168.5.0/24", "172.16.0.0/12"]}
-    builder = ubuntu_2104_individual_network(public=False, metadata=subnets)
+    builder = ubuntu_2204_individual_network(public=False, metadata=subnets)
     tasks = builder.render()
     result = dedent(
         """\
@@ -158,11 +158,11 @@ def test_ubuntu_2104_private_individual_task_etc_network_interfaces_with_custom_
     assert tasks["etc/network/interfaces"] == result
 
 
-def test_ubuntu_2104_etc_resolvers_configured(ubuntu_2104_individual_network, fake):
+def test_ubuntu_2204_etc_resolvers_configured(ubuntu_2204_individual_network, fake):
     """
     Validates /etc/resolv.conf is configured correctly
     """
-    builder = ubuntu_2104_individual_network()
+    builder = ubuntu_2204_individual_network()
     resolver1 = fake.ipv4()
     resolver2 = fake.ipv4()
     builder.network.resolvers = (resolver1, resolver2)
@@ -176,11 +176,11 @@ def test_ubuntu_2104_etc_resolvers_configured(ubuntu_2104_individual_network, fa
     assert tasks["etc/resolv.conf"] == result
 
 
-def test_ubuntu_2104_etc_hostname_configured(ubuntu_2104_individual_network):
+def test_ubuntu_2204_etc_hostname_configured(ubuntu_2204_individual_network):
     """
     Validates /etc/hostname is configured correctly
     """
-    builder = ubuntu_2104_individual_network()
+    builder = ubuntu_2204_individual_network()
     tasks = builder.render()
     result = dedent(
         """\
@@ -190,11 +190,11 @@ def test_ubuntu_2104_etc_hostname_configured(ubuntu_2104_individual_network):
     assert tasks["etc/hostname"] == result
 
 
-def test_ubuntu_2104_etc_hosts_configured(ubuntu_2104_individual_network):
+def test_ubuntu_2204_etc_hosts_configured(ubuntu_2204_individual_network):
     """
     Validates /etc/hosts is configured correctly
     """
-    builder = ubuntu_2104_individual_network()
+    builder = ubuntu_2204_individual_network()
     tasks = builder.render()
     result = dedent(
         """\
@@ -209,12 +209,12 @@ def test_ubuntu_2104_etc_hosts_configured(ubuntu_2104_individual_network):
     assert tasks["etc/hosts"] == result
 
 
-def test_ubuntu_2104_persistent_interface_names(ubuntu_2104_individual_network):
+def test_ubuntu_2204_persistent_interface_names(ubuntu_2204_individual_network):
     """
     When using certain operating systems, we want to bypass driver interface name,
     here we make sure the /etc/udev/rules.d/70-persistent-net.rules is generated.
     """
-    builder = ubuntu_2104_individual_network()
+    builder = ubuntu_2204_individual_network()
     tasks = builder.render()
     result = dedent(
         """\
@@ -237,14 +237,14 @@ def test_ubuntu_2104_persistent_interface_names(ubuntu_2104_individual_network):
     assert tasks["etc/udev/rules.d/70-persistent-net.rules"] == result
 
 
-def test_ubuntu_2104_public_individual_dhcp_task_etc_network_interfaces(
-    ubuntu_2104_individual_network,
+def test_ubuntu_2204_public_individual_dhcp_task_etc_network_interfaces(
+    ubuntu_2204_individual_network,
     make_interfaces_dhcp_metadata,
     expected_file_etc_network_interfaces_dhcp_2,
 ):
     """Validates /etc/network/interfaces for a public dhcp interfaces"""
 
-    builder = ubuntu_2104_individual_network(
+    builder = ubuntu_2204_individual_network(
         public=True, post_gen_metadata=make_interfaces_dhcp_metadata
     )
     tasks = builder.render()
@@ -254,13 +254,13 @@ def test_ubuntu_2104_public_individual_dhcp_task_etc_network_interfaces(
     assert tasks["etc/network/interfaces"] == result
 
 
-def test_ubuntu_2104_etc_resolvers_dhcp(
-    ubuntu_2104_individual_network, make_interfaces_dhcp_metadata
+def test_ubuntu_2204_etc_resolvers_dhcp(
+    ubuntu_2204_individual_network, make_interfaces_dhcp_metadata
 ):
     """
     Validates /etc/resolv.conf is skipped
     """
-    builder = ubuntu_2104_individual_network(
+    builder = ubuntu_2204_individual_network(
         post_gen_metadata=make_interfaces_dhcp_metadata
     )
     tasks = builder.render()
